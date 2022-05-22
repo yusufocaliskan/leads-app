5c60136b4d0e1cc0004f1c05ff5da1817badaa4c
yusufocaliskan <yusufocaliskan@gmail.com>
2022-05-22 10:46:58 +0300

    The folder three created

--- front/src/components/home.vue
+++ front/src/components/home.vue
@@ -14,7 +14,7 @@
                 <div class="flex items-center justify-between w-full md:w-auto">
                   <a href="#">
                     <span class="sr-only">Workflow</span>
-                    <img alt="Workflow" class="h-8 w-auto sm:h-10" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" />
+                    <img alt="Workflow" class="h-8 w-auto sm:h-10" src="../../public/logo.svg" />
                   </a>
                   <div class="-mr-2 flex items-center md:hidden">
                     <PopoverButton class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
