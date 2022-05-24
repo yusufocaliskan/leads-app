65077cda1cdc5f395b026d43e1e7232c6b8ea385
yusufocaliskan <yusufocaliskan@gmail.com>
2022-05-24 22:40:53 +0300

    Vue folder changed

---
+++ resources/src/utilities/LeadsAxios.js
@@ -0,0 +1,25 @@
+import axios from 'axios'
+import store from '../store'
+
+//Create a custom axios
+const LeadsAxios = axios.create({
+    baseURL: 'http://localhost:8000/api/',
+    headers:{
+        "Content-Type":'applications/json',
+        accept:'applications/json',
+        'X-Requested-With':'XMLHttpRequest'
+    }
+});
+
+
+//include the user token to the each request
+//it is a necessity of Laravel 
+LeadsAxios.interceptors.request.use(config =>{
+
+    config.headers.Authorization = `Bearer ${store.state.UserModule.admin_data.token}`
+    return config;
+})
+
+
+//Export
+export default LeadsAxios;
